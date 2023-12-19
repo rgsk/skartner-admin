4bbd0d471e63bf6055df27df4cb8efd78dfb736e
rgsk <guptarahul70322@gmail.com>
2023-12-20 03:07:43 +0530

    done

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,7 @@
 cp .env ../envs/skartner-admin/temp.env
 cp ../envs/skartner-admin/staging.env .env
 
+yarn
 yarn build
 
 cp ../envs/skartner-admin/temp.env .env
@@ -8,5 +9,3 @@ cp ../envs/skartner-admin/temp.env .env
 docker build \
  . -t rgskartner/skartner-admin \
  --platform linux/amd64
-
-docker push rgskartner/skartner-admin
